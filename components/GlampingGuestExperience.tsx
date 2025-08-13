35836c4368319a9aecbef0119d3ad89084d308b2
yakouben <your-email@example.com>
2025-08-13 20:34:54 +0100

    Change properties grid from 2 columns to 1 column on small screens

--- components/GlampingGuestExperience.tsx
+++ components/GlampingGuestExperience.tsx
@@ -596,7 +596,7 @@ export default function GlampingGuestExperience() {
                   </p>
                 </div>
               ) : (
-                <div className={`grid gap-4 sm:gap-6 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4`}>
+                <div className={`grid gap-4 sm:gap-6 grid-cols-1 lg:grid-cols-3 xl:grid-cols-4`}>
                   {currentProperties.map((property) => (
                     <div 
                       key={property.id} 
